21a0f507be453e7c38ea60e5c29e0ded83b73303
Clément Lesaege <clemage@live.fr>
2018-03-16 17:25:58 +0100

    Update CutAndChooseForEarlyReveal.sol

--- early-reveal-penalization/CutAndChooseForEarlyReveal.sol
+++ early-reveal-penalization/CutAndChooseForEarlyReveal.sol
@@ -1,1 +1,2 @@
 // TODO
+// Make a cut and choose attack contract following the description of the attack: https://docs.google.com/document/d/1KVUrjxUkVT01ekQHhDeILr5unJdHP-UFl_dkki-xCmE/edit?usp=sharing
